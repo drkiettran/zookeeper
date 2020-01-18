5e5b26c2a5d99f349c93b6dc437b3697506b624d
drkiettran <ktran@harrisburu.edu>
2020-01-18 14:08:06 -0500

    Added example code Master

---
+++ src/main/java/com/drkiettran/zookeeper/Master.java
@@ -0,0 +1,30 @@
+package com.drkiettran.zookeeper;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Master implements Watcher {
+	ZooKeeper zk;
+	String hostPort;
+
+	Master(String hostPort) {
+		this.hostPort = hostPort;
+	}
+
+	void startZK() throws IOException {
+		zk = new ZooKeeper(hostPort, 15000, this);
+	}
+
+	public void process(WatchedEvent e) {
+		System.out.println(e);
+	}
+
+	public static void main(String args[]) throws Exception {
+		Master m = new Master(args[0]);
+		m.startZK();
+		Thread.sleep(60000);
+	}
+}
